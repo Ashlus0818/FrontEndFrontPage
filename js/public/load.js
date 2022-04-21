a51b7bd04e924067abb79481bcbb14837ef2fca1
milkyeway <namilkywaw@gmail.com>
2022-04-21 16:05:21 +0800

    1

---
+++ js/public/load.js
@@ -0,0 +1,9 @@
+$(function () {
+  /*公共部分
+   * 導航欄
+   * footer CopyRight
+   */
+  $("#headerpage").load("../../html/public/header.html");
+  $("#footerpage").load("../../html/public/footer.html");
+  $("#sliderpage").load("../../html/public/slider.html");
+});
