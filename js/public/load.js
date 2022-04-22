278eda0dfb4e09c4ba5593da43c8e5ddf72eb341
milkyeway <namilkywaw@gmail.com>
2022-04-22 13:59:24 +0800

    尚未變更目錄

--- js/public/load.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  /*公共部分
-   * 導航欄
-   * footer CopyRight
-   */
-  $("#headerpage").load("../../html/public/header.html");
-  $("#footerpage").load("../../html/public/footer.html");
-  $("#sliderpage").load("../../html/public/slider.html");
-});
